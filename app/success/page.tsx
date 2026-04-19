fec7c287cc0f51321bff0e5654d969515360d229
Ranjeet Singhe <ranjeetdynamic5@gmail.com>
2026-04-19 21:46:02 +0530

    fix success page loading issue

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -20,9 +20,23 @@ export default function SuccessPage() {
   if (!orderId) return <p>Loading...</p>;
 
   return (
-    <div>
-      <h1>Order Success</h1>
-      <p>{orderId}</p>
+  <div className="min-h-screen flex items-center justify-center bg-[#f8fafc]">
+    <div className="bg-white p-10 rounded-xl shadow-md text-center">
+      <h1 className="text-2xl font-bold text-green-600 mb-2">
+        ✅ Order Successful
+      </h1>
+      <p className="text-gray-600 mb-4">
+        Thank you for your purchase!
+      </p>
+      <p className="font-semibold text-lg">{orderId}</p>
+
+      <a
+        href="/products"
+        className="inline-block mt-6 px-6 py-3 bg-[#D9534F] text-white rounded-lg hover:bg-[#c9302c]"
+      >
+        Continue Shopping
+      </a>
     </div>
-  );
+  </div>
+);
 }
